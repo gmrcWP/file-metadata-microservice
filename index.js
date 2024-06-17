b38f5642544b87a30c6b61ab301e698592eae10a
gmrcWP <jorgesick.roch@gmail.com>
2024-06-16 22:37:56 -0400

    feat/filemetadata solution added

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
+const upload = multer();
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  const { originalname, size, mimetype } = req.file;
+  res.send({ name: originalname, type: mimetype, size: size });
+});
 
 
 const port = process.env.PORT || 3000;
